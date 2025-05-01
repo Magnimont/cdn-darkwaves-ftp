2deb7344a280db2937fac1391f44c940a6529f87
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2025-05-01 18:06:22 +0530

    up

--- video_detect.py
+++ video_detect.py
@@ -1,4 +1,3 @@
-import cv2
 import os
 
 
@@ -7,29 +6,9 @@ def check_video(video_path: str) -> dict:
     if not video_path or not os.path.exists(video_path):
         return False
 
-    cap = cv2.VideoCapture(video_path)
-    if not cap.isOpened():
-        return False
-
     isNudeContent = False
     isAdultContent = False
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # Convert the frame to an image file-like object
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame_path = 'frame.jpg'
-        with open(frame_path, 'wb') as f:
-            f.write(buffer)
-
-
-        # Remove the temporary frame file
-        os.remove(frame_path)
-
-    cap.release()
 
     return {"isNudeContent": isNudeContent, "isAdultContent": isAdultContent}
 
